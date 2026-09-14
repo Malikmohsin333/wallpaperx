799f95c4f5166aaf6605c0f954388a690bcf086c
Malikmohsin333 <malikmohsinics1@gmail.com>
2026-09-14 20:53:08 +0500

    Expand API service for wallpaper categories

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -57,6 +57,54 @@ class ApiService {
 
     final photos = response.data['photos'] as List;
 
+        return photos
+        .map(
+          (photo) => Wallpaper.fromJson(
+            photo as Map<String, dynamic>,
+          ),
+        )
+        .toList();
+  }
+
+  Future<List<Wallpaper>> getWallpapers({
+    required String category,
+    int page = 1,
+    int perPage = 15,
+  }) async {
+    String endpoint;
+
+    if (category == 'Curated') {
+      endpoint =
+          'https://api.pexels.com/v1/curated?per_page=$perPage&page=$page&orientation=portrait';
+    } else if (category == '4K Ultra HD') {
+      endpoint =
+          'https://api.pexels.com/v1/search?query=4k+wallpaper&per_page=$perPage&page=$page';
+    } else if (category == 'Trending') {
+      endpoint =
+          'https://api.pexels.com/v1/curated?per_page=$perPage&page=$page&orientation=portrait';
+    } else if (category == 'New') {
+      endpoint =
+          'https://api.pexels.com/v1/curated?per_page=$perPage&page=${page + 20}&orientation=portrait';
+    } else if (category == 'Random') {
+      final seed = (DateTime.now().millisecondsSinceEpoch % 15) + page;
+      endpoint =
+          'https://api.pexels.com/v1/curated?per_page=$perPage&page=$seed&orientation=portrait';
+    } else {
+      endpoint =
+          'https://api.pexels.com/v1/search?query=$category&per_page=$perPage&page=$page';
+    }
+
+    final response = await _dio.get(
+      endpoint,
+      options: Options(
+        headers: {
+          'Authorization': _apiKey,
+        },
+      ),
+    );
+
+    final photos = response.data['photos'] as List;
+
     return photos
         .map(
           (photo) => Wallpaper.fromJson(
